5e802e2d085feca472ce9a70a5aa2121675a88f1
arjun2010-ece <arjun2010.ece@gmail.com>
2019-11-26 19:27:47 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css"
 function App() {
   return (
     <div className='container mt-5 py-5'>
+	<p>A quiz application</p>
       <div className="row">
          <div className="col-md-6 offset-md-3">
             <Quiz />
